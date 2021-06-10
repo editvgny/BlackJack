6e4bac7844644d2e71d20c3d5b00571b67b8e534
editvgny <edit.vgny@gmail.com>
2021-06-10 10:04:33 +0200

    Formatted, added block comments, removed blank lines from PointCounter.php

--- PointCounter.php
+++ PointCounter.php
@@ -1,30 +1,36 @@
 <?php
 
 
-class PointCounter
-{
-    public static function bestPointCounter($deck)
-    {
+class PointCounter {
+    /**
+     * @param $deck
+     * @return float|int|mixed
+     */
+    public static function bestPointCounter($deck) {
         $point = 0;
         foreach ($deck as $card) {
             $point += $card->getPoint();
         }
         if ($point > 21) {
-            if(self::getNumberOFAce($deck) == 1){
+            if (self::getNumberOFAce($deck) == 1) {
                 $point -= 10;
                 return $point;
-            }elseif(self::getNumberOFAce($deck) > 1){
-                $point -= (self::getNumberOFAce($deck)-1)*10;
-                if($point < 22){
-                    return $point;}
+            } elseif (self::getNumberOFAce($deck) > 1) {
+                $point -= (self::getNumberOFAce($deck) - 1) * 10;
+                if ($point < 22) {
+                    return $point;
+                }
                 return $point - 10;
             }
         }
         return $point;
     }
 
-    public static function getNumberOFAce($deck)
-    {
+    /**
+     * @param $deck
+     * @return int
+     */
+    public static function getNumberOFAce($deck): int {
         $counter = 0;
         foreach ($deck as $card) {
             if ($card->getValue() == "A") {
@@ -33,8 +39,4 @@ class PointCounter
         }
         return $counter;
     }
-
-
-
-
 }
