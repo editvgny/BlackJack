b72da938501a26d7f0fcbef072fc1fb19f5a6acc
editvgny <edit.vgny@gmail.com>
2021-06-10 10:02:01 +0200

    Formatted, added block comments, removed blank lines from CardDeck.php

--- card/CardDeck.php
+++ card/CardDeck.php
@@ -1,37 +1,43 @@
 <?php
 
 
-class CardDeck
-{
+class CardDeck {
     private $deck = array();
 
-    public function addCardToDeck($card){
-        array_push($this->deck,$card);
+    /**
+     * @param $card
+     */
+    public function addCardToDeck($card) {
+        array_push($this->deck, $card);
     }
 
-    public function getCards(){
-        foreach ($this->deck as $card){
+    public function getCards() {
+        foreach ($this->deck as $card) {
             $card->getCard();
         }
     }
 
-    public function dealCard($index){
+    /**
+     * @param $index
+     * @return mixed
+     */
+    public function dealCard($index) {
         $randomCard = $this->deck[$index];
         unset($this->deck[$index]);
         return $randomCard;
     }
 
-
     /**
      * @return array
      */
-    public function getDeck(): array
-    {
+    public function getDeck(): array {
         return $this->deck;
     }
 
-
-    public function getNumberOfCardsInDeck(){
+    /**
+     * @return int
+     */
+    public function getNumberOfCardsInDeck(): int {
         return count($this->deck);
     }
 }
