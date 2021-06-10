e7c663374b16ee09ed5f8b8fa6ba0e91ca6983f7
editvgny <edit.vgny@gmail.com>
2021-06-10 10:02:50 +0200

    Formatted, removed blank lines and unused variable from Dealer.php

--- player/Dealer.php
+++ player/Dealer.php
@@ -1,58 +1,53 @@
 <?php
 
 
-class Dealer
-{
-    private $name = "Dealer";
+class Dealer {
     private $cards = array();
     private $hide = true;
-
 
     /**
      * @return null[]
      */
-    public function getCards(): array
-    {
+    public function getCards(): array {
         return $this->cards;
     }
 
-    public function addCard($card)
-    {
+    /**
+     * @param $card
+     */
+    public function addCard($card) {
         array_push($this->cards, $card);
     }
 
-    public function dealerPoint()
-    {
-
+    /**
+     * @return float|int|mixed
+     */
+    public function dealerPoint() {
         return PointCounter::bestPointCounter($this->cards);
     }
 
-
-    public function deleteCard()
-    {
+    public function deleteCard() {
         $this->cards = array();
     }
 
-    public function isCardNeeded()
-    {
+    public function isCardNeeded(): bool {
         if ($this->dealerPoint() < 17) {
             return true;
         }
         return false;
     }
 
-
-    public function isHide()
-    {
+    /**
+     * @return bool
+     */
+    public function isHide(): bool {
         return $this->hide;
-
     }
 
     /**
      * @param bool $hide
      */
-    public function setHide(bool $hide)
-    {
+    public function setHide(bool $hide) {
         $this->hide = $hide;
     }
 }
