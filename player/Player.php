224ec7b7131b584301a48c3bef06395e8d951ad1
editvgny <edit.vgny@gmail.com>
2021-06-10 10:03:45 +0200

    Formatted, removed blank lines from Player.php

--- player/Player.php
+++ player/Player.php
@@ -1,66 +1,61 @@
 <?php
 
 
-class Player
-{
+class Player {
     private $name = "";
     private $cards = array();
     private $cash = 1000;
 
-
     /**
      * @param string $name
      */
-    public function setName(string $name)
-    {
+    public function setName(string $name) {
         $this->name = $name;
     }
 
     /**
      * @return string
      */
-    public function getName(): string
-    {
+    public function getName(): string {
         return $this->name;
     }
 
     /**
      * @return int
      */
-    public function getCash(): int
-    {
+    public function getCash(): int {
         return $this->cash;
     }
 
     /**
      * @return null[]
      */
-    public function getCards()
-    {
+    public function getCards(): array {
         return $this->cards;
     }
 
-    public function addCard($card){
-        array_push($this->cards,$card);
+    /**
+     * @param $card
+     */
+    public function addCard($card) {
+        array_push($this->cards, $card);
     }
 
     /**
      * @param int $cash
      */
-    public function setCash(int $cash)
-    {
-        $this->cash = $this->cash+$cash;
+    public function setCash(int $cash) {
+        $this->cash = $this->cash + $cash;
     }
 
-    public function deleteCard(){
+    public function deleteCard() {
         $this->cards = array();
     }
 
-
-    public function playerPoint(){
-
+    /**
+     * @return float|int|mixed
+     */
+    public function playerPoint() {
         return PointCounter::bestPointCounter($this->cards);
     }
-
-
 }
