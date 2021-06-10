5d01a20b8e8f3606c4087ff533bd54589032f82a
editvgny <edit.vgny@gmail.com>
2021-06-10 10:01:37 +0200

    Formatted, added block comments, removed blank lines and out-commented lines from Card.php

--- card/Card.php
+++ card/Card.php
@@ -1,68 +1,50 @@
 <?php
 
 
-class Card
-{
+class Card {
     private $form;
     private $value;
     private $point;
-    private $design=array();
 
-
-    public function __construct($form, $value)
-    {
+    /**
+     * Card constructor.
+     * @param $form
+     * @param $value
+     */
+    public function __construct($form, $value) {
         $this->form = $form;
         $this->value = $value;
-        if($value == "J" || $value == "Q" || $value == "K" ){
+        if ($value == "J" || $value == "Q" || $value == "K") {
             $this->point = 10;
-        } elseIf($value == "A"){
+        } elseif ($value == "A") {
             $this->point = 11;
-        }else{
+        } else {
             $this->point = $value;
         }
-
-
-//        array_push($this->design,'┌──────────┐');
-//        array_push($this->design,'│ '."\033[31m".$value.'♠'."\033[37m".'       │');
-//        array_push($this->design,'│          │');
-//        array_push($this->design,'│    '."\033[31m".json_decode('"'.$form.'"')."\033[37m".'     │');
-//        array_push($this->design,'│          │');
-//        array_push($this->design,'│       '."\033[31m".$value.json_decode('"'.$form.'"')."\033[37m".' │');
-//        array_push($this->design,'└──────────┘');
-
-//        foreach ($this->design as $row){
-//            echo $row."\n";
-//        }
     }
 
-
-    public function getCard(){
-        echo $this->form."-".$this->value."\n";
+    public function getCard() {
+        echo $this->form . "-" . $this->value . "\n";
     }
 
     /**
      * @return mixed
      */
-    public function getForm()
-    {
+    public function getForm() {
         return $this->form;
     }
 
-
     /**
      * @return mixed
      */
-    public function getValue()
-    {
+    public function getValue() {
         return $this->value;
     }
-
 
     /**
      * @return int
      */
-    public function getPoint(): int
-    {
+    public function getPoint(): int {
         return $this->point;
     }
 }
